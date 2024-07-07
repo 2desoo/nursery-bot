69f7469c3ff6e4f3a5d557f7ab82aff6aa76e184
2desoo <marcus60.2001@gmail.com>
2024-07-07 23:00:21 +0300

    Initial commit

---
+++ src/main/java/nursery/NurseryBotApplication.java
@@ -0,0 +1,13 @@
+package nursery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nursery/NurseryBotApplicationTests.java
@@ -0,0 +1,13 @@
+package nursery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NurseryBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
