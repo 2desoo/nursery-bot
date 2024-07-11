56e2a1f6a8ed588d7319c6a37662225fe4d3a7f1
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-07-11 17:58:55 +0300

    Info and WorkDays ver2

--- src/main/java/nursery/service/TelegramBot.java
+++ src/main/java/nursery/service/TelegramBot.java
@@ -7,9 +7,11 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.commands.SetMyCommands;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.commands.BotCommand;
 import org.telegram.telegrambots.meta.api.objects.commands.scope.BotCommandScopeDefault;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
@@ -57,17 +59,34 @@ public class TelegramBot extends TelegramLongPollingBot {
                     workdaysCommandReceived(chatId, update.getMessage().getChat().getFirstName());
                     break;
                 default:
-                    sendMassage(chatId, "Not commands.");
+                    sendMassage(chatId, "Not commands.", null);
+            }
+
+
+        } else if (update.hasCallbackQuery()) {
+            Long chatId = update.getCallbackQuery().getMessage().getChatId();
+            if(update.getCallbackQuery().getData().equals("/start")) {
+                startCommandReceivedTest(chatId, "TestStart");
+            } else if (update.getCallbackQuery().getData().equals("/info")) {
+                infoCommandReceived(chatId, "TestInfo");
             }
         }
     }
 
-    private void startCommandReceived(Long chatId, String name) {
+    private void startCommandReceived(Long chatId, String name  ) {
 
         String answer = "Привет " + name + ".\n" +
                         config.getStartText();
 
-        sendMassage(chatId, answer);
+        sendMassage(chatId, answer, createKeyboard1());
+    }
+
+    private void startCommandReceivedTest(Long chatId, String name) {
+
+        String answer = "Привет " + name + ".\n" +
+                config.getStartText();
+
+        sendMassage(chatId, answer, null);
     }
 
     private void infoCommandReceived(Long chatId, String name) {
@@ -75,7 +94,7 @@ public class TelegramBot extends TelegramLongPollingBot {
         String answer = "Привет " + name + ". info test. \n" +
                 config.getInfoText();
 
-        sendMassage(chatId, answer);
+        sendMassage(chatId, answer, null);
     }
 
     private void workdaysCommandReceived(Long chatId, String name) {
@@ -83,20 +102,47 @@ public class TelegramBot extends TelegramLongPollingBot {
         String answer = "Привет " + name + ".\n" +
                 config.getWorkDays();
 
-        sendMassage(chatId, answer);
+        sendMassage(chatId, answer, null );
     }
 
-    private void sendMassage(Long chatId, String textToSend) {
+    private void sendMassage(Long chatId, String textToSend,InlineKeyboardMarkup createKeyboard1) {
 
         SendMessage massage = new SendMessage();
         massage.setChatId(String.valueOf(chatId));
         massage.setText(textToSend);
+
+        massage.setReplyMarkup(createKeyboard1);
 
         try {
             execute(massage);
         } catch (TelegramApiException e) {
             logger.error("Error: " + e.getMessage());
         }
+    }
+
+    private EditMessageReplyMarkup createEditMessageReplyMarkup(Update update, InlineKeyboardMarkup replyMarkup) {
+        EditMessageReplyMarkup editMessageReplyMarkup = new EditMessageReplyMarkup();
+        editMessageReplyMarkup.setReplyMarkup(replyMarkup);
+        return editMessageReplyMarkup;
+    }
+
+    // Main keyboard
+    private InlineKeyboardMarkup createKeyboard1() {
+        InlineKeyboardMarkup keyboardMarkup = new InlineKeyboardMarkup();
+        List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+        List<InlineKeyboardButton> row = new ArrayList<>();
+        row.add(createButtonWithCallbackData("Button #1", "/start"));
+        row.add(createButtonWithCallbackData("Button #2", "/info"));
+        keyboard.add(row);
+        keyboardMarkup.setKeyboard(keyboard);
+        return keyboardMarkup;
+    }
+
+    private static InlineKeyboardButton createButtonWithCallbackData(String text, String callbackData) {
+        InlineKeyboardButton button = new InlineKeyboardButton();
+        button.setText(text);
+        button.setCallbackData(callbackData);
+        return button;
     }
 
 
