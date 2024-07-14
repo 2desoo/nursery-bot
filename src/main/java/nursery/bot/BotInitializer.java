af14814f09f9aae3b76111cb9162bdeba71bcfd6
Sergei-Bashinskii <sergei13121390@gmail.com>
2024-07-14 22:35:16 +0300

    Shelter_0.2

--- src/main/java/nursery/bot/BotInitializer.java
+++ src/main/java/nursery/bot/BotInitializer.java
@@ -1,32 +1,26 @@
 package nursery.bot;
 
 import nursery.configuration.TelegramBot;
-import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
-import static org.slf4j.LoggerFactory.*;
-
-@Component
+@Configuration
 public class BotInitializer {
 
     @Autowired
-    TelegramBot bot;
-
-    private final Logger logger = getLogger(BotInitializer.class);
+    TelegramBot telegramBot;
 
     @EventListener({ContextRefreshedEvent.class})
     public void init() throws TelegramApiException {
         TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
         try {
-            telegramBotsApi.registerBot(bot);
+            telegramBotsApi.registerBot(telegramBot);
         } catch (TelegramApiException e) {
-            logger.error("Error: {}" + e.getMessage());
         }
     }
 }
