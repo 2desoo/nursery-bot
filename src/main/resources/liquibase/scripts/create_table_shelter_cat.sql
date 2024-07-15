e6cb113071f1351a605950c28e033a4821bf1111
2desoo <marcus60.2001@gmail.com>
2024-07-15 21:44:05 +0300

    Fixed sql2

--- src/main/resources/liquibase/scripts/create_table_shelter_cat.sql
+++ src/main/resources/liquibase/scripts/create_table_shelter_cat.sql
@@ -1,23 +1,9 @@
 -- formated sql
 
 -- changeset dubrovsky:2
- CREATE TABLE shelterCat
- (
-     id              BIGSERIAL NOT NULL,
-     name varchar,
-     welcomes_user_shelter varchar,
-     tell_about_shelter varchar,
-     schedule_work_shelter varchar,
-     address_shelter varchar,
-     contact_information_security varchar,
-     safety_recommendations varchar,
- );
+ CREATE TABLE shelterCat(id BIGSERIAL, name varchar, welcomes_user_shelter varchar,
+ tell_about_shelter varchar, schedule_work_shelter varchar, address_shelter varchar,
+ contact_information_security varchar, safety_recommendations varchar)
 
- CREATE TABLE travelMap
- (
-    id BIGSERIAL NOT NULL,
-    file_path VARCHAR,
-    file_size BIGSERIAL,
-    media_type VARCHAR,
-    date TIMESTAMP,
- );
+ CREATE TABLE travelMap(id BIGSERIAL, file_path VARCHAR, file_size BIGSERIAL,
+ media_type VARCHAR, date TIMESTAMP)
